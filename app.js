03088953724b71eb7a1a1eb5e8d34bfdaed3ac79
mayankagg9722 <mayankagg9722@gmail.com>
2017-01-12 16:35:22 +0530

    hjs template added

--- app.js
+++ app.js
@@ -118,7 +118,7 @@ var free_slots=[];
 //@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.set('view engine', 'hjs');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
